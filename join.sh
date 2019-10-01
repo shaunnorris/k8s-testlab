ad17a60271df2a9c81680deb74565796c0b5646c
shaunnorris@gmail.com <47999629+shaun-pivotal@users.noreply.github.com>
2019-10-01 15:55:24 +1000

    added a path variable to vagrantfile
    - improvement to vagrantfile - path variable added

--- join.sh
+++ join.sh
@@ -1,2 +1,2 @@
-kubeadm join 10.0.0.10:6443 --token p9bygp.kc2g7xz9tyqj46ua \
-    --discovery-token-ca-cert-hash sha256:427299163ca101c4f10ab67ef0f73aeed516e7333f919ec4ddbdf28519733caa 
+kubeadm join 10.0.0.10:6443 --token wdvorl.j274z1i7u2y7fn8w \
+    --discovery-token-ca-cert-hash sha256:5512ba27073efc8fccd6530cbe3615d40d15361506882460e6adb56200e8ea45 
