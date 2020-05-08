f513fa336563431aa50ab90e1689fcff23cced12
Shaun Norris <shaunnorris@gmail.com>
2020-05-08 03:42:37 +0000

    fixed testing and default values for cluster name and worker node count

--- kubeadm-cluster.sh
+++ kubeadm-cluster.sh
@@ -3,7 +3,7 @@
 #initial VM setup
 
 #TODO LIST
-# get cpu / ran / disk size for primary from command line with sensible defaults
+# get cpu / ram / disk size for primary from command line with sensible defaults
 # get cpu / ram / disk size for nodes from command line with sensible defaults
 # check if nodes with same name as what will be built already exist and quit with a warning
 # install cluster with kubeadm
@@ -15,25 +15,22 @@
 # check if multipass installed and quit if not
 command -v multipass >/dev/null 2>&1 || { echo >&2 "Please install multipass on your system before proceeding. https://multipass.run"; exit 1; }
 MPASS=`command -v multipass`
-echo "We will use $MPASS to run this script. Proceeding..." 
 
 # get a unique cluster name from command line entry
-read -p "Please enter a name for your cluster:" CLUSTERNAME
+DEFAULTNAME="testcluster"
+read -p "Please enter a name for your cluster: " -i "$DEFAULTNAME" -e CLUSTERNAME
 while [[ "$CLUSTERNAME" =~ [^a-zA-Z] ]] ; do
   echo "invalid cluster Name - please use alpha chars only"
-  read -p "Please enter a name for your cluster:" CLUSTERNAME
+  read -p "Please enter a name for your cluster: " -i "$DEFAULTNAME" -e CLUSTERNAME
 done
-echo "We will proceed with $CLUSTERNAME to identify VMs brought up with this script" 
 
 # get number of nodes from command line
-read -p "Please enter number of worker nodes(1-9):" CLUSTERNODES
+DEFAULTNODES=2
+read -p "Please enter number of worker nodes(1-9): " -i "$DEFAULTNODES" -e CLUSTERNODES
 while  ! (($CLUSTERNODES > 0 && $CLUSTERNODES < 10)) ; do
-  echo "please enter a number betweeen 1-9"
-  read -p "Please enter number of worker nodes(1-9):" CLUSTERNODES
+  echo "invalid entry - choose value 1-9"
+  read -p "Please enter number of worker nodes(1-9): " -i "$DEFAULTNODES" -e CLUSTERNODES
 done
-echo "We will build $CLUSTERNODES worker nodes in the $CLUSTERNAME cluster."
-echo "  along with 1 primary control plane vm"
-echo "  (This script currently does not build HA control plane clusters) "
 echo "==========="
 
 
