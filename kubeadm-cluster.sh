251b40ab7b8a1ab81d253e40eee5f7cfdd6b0e00
Shaun Norris <shaunnorris@gmail.com>
2020-05-08 01:54:33 +0000

    added todo list to kubeadm-cluster build script

--- kubeadm-cluster.sh
+++ kubeadm-cluster.sh
@@ -2,6 +2,24 @@
 
 #initial VM setup
 
+#TODO LIST
+# check if multipass installed
+# get a unique cluster name from command line entry
+# get number of nodes from command line
+# get cpu / ran / disk size for primary from command line with sensible defaults
+# get cpu / ram / disk size for nodes from command line with sensible defaults
+# check if nodes with same name as what will be built already exist and quit with a warning
+# install cluster with kubeadm
+# install metallb - and ask for what IP range should be shared - with sensible defaults
+# install local-path default storage path from rancher
+# install KOV and setup to use LB
+# add a delete option to specifiy a cluster name and delete all multipass nodes including that name, and delete them
+
+
+
+
+
+
 SHAREPATH="$PWD/multipass"
 
 
